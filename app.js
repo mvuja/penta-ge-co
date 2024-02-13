21b496239158612b6b1ca401678b63ea619544fa
unknown <markowujanovic@gmail.com>
2024-02-13 16:27:27 +0100

    continue home page

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ window.addEventListener('scroll', () => {
     }
 })
 
+
+
+// HIDE-SHOW HEADER ON SCROLL
+let prevScrollpos = window.scrollY
+let headerHeight = mainHeader.offsetHeight
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.scrollY
+    headerHeight = mainHeader.offsetHeight
+    if (prevScrollpos > currentScrollPos) {
+        mainHeader.style.top = '0'
+    } else {
+        mainHeader.style.top = `-${headerHeight}px`
+    }
+    prevScrollpos = currentScrollPos
+})
+
+
+
 // HAMBURGER MENU
 const hamburger = document.querySelector('.hamburger')
 const navbar = document.querySelector('.nav-links')
@@ -49,3 +67,58 @@ document.addEventListener('mousemove', function(e) {
     cursor.style.left = (e.pageX - 25) + 'px'
     cursor.style.top = (e.pageY - 25) + 'px'
 })
+
+
+
+
+
+
+// TEXT REVEAL ON SCROLL
+// gsap.config({ trialWarn: false });
+// console.clear();
+// gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
+// let smoother = ScrollSmoother.create({
+//   smooth: 2
+// });
+
+// let split = new SplitText("h1", { type: "lines" });
+// let masks;
+// function makeItHappen() {
+//   masks = [];
+//   split.lines.forEach((target) => {
+//     let mask = document.createElement("span");
+//     mask.className = "mask";
+//     target.append(mask);
+//     masks.push(mask);
+//     gsap.to(mask, {
+//       scaleX: 0,
+//       transformOrigin: "right center",
+//       ease: "none",
+//       scrollTrigger: {
+//         trigger: target,
+//         markers: {
+//           startColor: "white",
+//           endColor: "#42a6e0",
+//           fontSize: "12px",
+//           indent: 10
+//         },
+//         scrub: true,
+//         start: "top center",
+//         end: "bottom center"
+//       }
+//     });
+//   });
+// }
+
+// window.addEventListener("resize", newTriggers);
+
+// function newTriggers() {
+//   ScrollTrigger.getAll().forEach((trigger, i) => {
+//     trigger.kill();
+//     masks[i].remove();
+//   });
+//   split.split();
+//   makeItHappen();
+// }
+
+// makeItHappen();

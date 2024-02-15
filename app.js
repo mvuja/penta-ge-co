597b4af07d14faf2a01424d944d6143f59e49c72
unknown <markowujanovic@gmail.com>
2024-02-15 08:31:23 +0100

    add gsap libraries

--- app.js
+++ app.js
@@ -74,7 +74,8 @@ document.addEventListener('mousemove', function(e) {
 
 
 // COUNTER ANIMATION - REFERENCE
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+import {SplitText} from "./SplitText.js"
 
 const items = document.querySelectorAll("#reference .data")
 
@@ -104,51 +105,39 @@ function numberWithCommas(x) {
 
 
 // TEXT REVEAL ON SCROLL
-// gsap.config({ trialWarn: false });
-// console.clear();
-// gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
-// let smoother = ScrollSmoother.create({
-//   smooth: 2
-// });
+// gsap.config({ trialWarn: false })
+let smoother = ScrollSmoother.create({
+  smooth: 1,
+  content: "main",
+})
 
-// let split = new SplitText("h1", { type: "lines" });
-// let masks;
-// function makeItHappen() {
-//   masks = [];
-//   split.lines.forEach((target) => {
-//     let mask = document.createElement("span");
-//     mask.className = "mask";
-//     target.append(mask);
-//     masks.push(mask);
-//     gsap.to(mask, {
-//       scaleX: 0,
-//       transformOrigin: "right center",
-//       ease: "none",
-//       scrollTrigger: {
-//         trigger: target,
-//         markers: {
-//           startColor: "white",
-//           endColor: "#42a6e0",
-//           fontSize: "12px",
-//           indent: 10
-//         },
-//         scrub: true,
-//         start: "top center",
-//         end: "bottom center"
-//       }
-//     });
-//   });
-// }
+let split = new SplitText(".fill-txt h2", { type: "lines" })
+let masks
+function makeItHappen() {
+  masks = []
+  split.lines.forEach((target) => {
+    let mask = document.createElement("span")
+    mask.className = "mask"
+    target.append(mask)
+    masks.push(mask)
+    gsap.to(mask, {
+      scaleX: 0,
+      transformOrigin: "right center",
+      ease: "none",
+      scrollTrigger: {
+        trigger: target,
+        // markers: {
+        //   startColor: "white",
+        //   endColor: "#42a6e0",
+        //   fontSize: "12px",
+        //   indent: 10
+        // },
+        scrub: true,
+        start: "top center",
+        end: "bottom center"
+      }
+    })
+  })
+}
 
-// window.addEventListener("resize", newTriggers);
-
-// function newTriggers() {
-//   ScrollTrigger.getAll().forEach((trigger, i) => {
-//     trigger.kill();
-//     masks[i].remove();
-//   });
-//   split.split();
-//   makeItHappen();
-// }
-
-// makeItHappen();
+makeItHappen()

416c39cec04a8c7821940217ed61459f9efa7903
unknown <markowujanovic@gmail.com>
2024-02-20 09:24:54 +0100

    continue responsive

--- app.js
+++ app.js
@@ -246,6 +246,18 @@ const newsSwiper = new Swiper('.news-swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
-
+  breakpoints: {
+    // when window width is >= 320px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    620: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+  },
 })

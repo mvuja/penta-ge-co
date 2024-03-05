bb3ac9792492229cc2405ee0f1f50e89577ddaff
unknown <markowujanovic@gmail.com>
2024-03-05 12:11:57 +0100

    add contact page

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function makeItHappen() {
         //   indent: 10
         // },
         // markers: true,
-        scrub: .5,
+        scrub: true,
         start: `${startPer}% center`,
         end: `${startPer + 200}% center`
       }
@@ -305,3 +305,96 @@ const projectsSwiper = new Swiper('.projects-swiper', {
     },
   },
 })
+
+
+
+
+
+
+
+
+// FLOATING LABELS
+
+const inputs = document.querySelectorAll('.input-field')
+
+
+inputs.forEach(el => {
+  const input = el.querySelector('.ip')
+  const label = el.querySelector('label')
+
+  input.addEventListener('input', () => {
+    if(input.value !== ''){
+      label.classList.add('active')
+    }else{
+      label.classList.remove('active')
+    }
+  })
+})
+
+
+
+
+
+
+
+
+
+
+// CUSTOM DROPDOWN
+const selectedAll = document.querySelectorAll(".wrapper-dropdown")
+
+selectedAll.forEach((selected) => {
+  const optionsList = selected.querySelectorAll("div.wrapper-dropdown li")
+
+  selected.addEventListener("click", () => {
+    const arrow = selected.children[1]
+
+    if (selected.classList.contains("active")) {
+      handleDropdown(selected, arrow, false)
+    } else {
+      let currentActive = document.querySelector(".wrapper-dropdown.active")
+
+      if (currentActive) {
+        let anotherArrow = currentActive.children[1]
+        handleDropdown(currentActive, anotherArrow, false)
+      }
+
+      handleDropdown(selected, arrow, true)
+    }
+  })
+
+  // update the display of the dropdown
+  for (let o of optionsList) {
+    o.addEventListener("click", () => {
+      selected.querySelector(".selected-display").innerHTML = o.innerHTML
+    })
+  }
+})
+
+// check if anything else ofther than the dropdown is clicked
+window.addEventListener("click", function (e) {
+  if (e.target.closest(".wrapper-dropdown") === null) {
+    closeAllDropdowns()
+  }
+})
+
+// close all the dropdowns
+function closeAllDropdowns() {
+  const selectedAll = document.querySelectorAll(".wrapper-dropdown")
+  selectedAll.forEach((selected) => {
+    let arrow = selected.children[1]
+
+    handleDropdown(selected, arrow, false)
+  })
+}
+
+// open all the dropdowns
+function handleDropdown(dropdown, arrow, open) {
+  if (open) {
+    arrow.classList.add("rotated")
+    dropdown.classList.add("active")
+  } else {
+    arrow.classList.remove("rotated")
+    dropdown.classList.remove("active")
+  }
+}
